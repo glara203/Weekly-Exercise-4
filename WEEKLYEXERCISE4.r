f1b19ef4314e019235033e3e1f39f16b379d47f2
Gabby Lara Larimore <glara@uic.edu>
2025-09-20 14:53:45 -0500

    Answered 6a

--- WEEKLYEXERCISE4.r
+++ WEEKLYEXERCISE4.r
@@ -1,2 +1,18 @@
 ## default branch
 ## [1] "main"
+
+#install and load libraries that I will need 
+install.packages("readr")
+library(tidyverse)
+library(ggplot2)  
+library(readr)
+# set wd
+
+#read in data set
+oly = read_csv("Olympics.csv")
+
+#a. Calculate a new variable, called ‘total.medals’, which is the sum of gold, silver, and bronze, and add it to the Olympic dataset. 
+
+total.medals <- oly$gold + oly$silver + oly$bronze #created new variable
+oly <- data.frame(oly, total.medals) #added variable to olympics data set
+view(oly) #double checked my work
