4aed0ff710b88cc8ac4317b8b67131dbc40e34af
Gabby Lara Larimore <glara@uic.edu>
2025-09-20 15:24:33 -0500

    load libraries and data

--- WEEKLYEXERCISE4.r
+++ WEEKLYEXERCISE4.r
@@ -1,2 +1,12 @@
 ## default branch
 ## [1] "main"
+
+#install and load libraries that I will need 
+install.packages("readr")
+library(tidyverse)
+library(ggplot2)  
+library(readr)
+
+#read in data set
+oly = read_csv("Olympics.csv")
+
