2fca07b0eaa6adeaf97c287228e9cd6c0e9cede0
Gabby Lara Larimore <glara@uic.edu>
2025-09-20 15:30:36 -0500

    answered 7a

--- WEEKLYEXERCISE4.r
+++ WEEKLYEXERCISE4.r
@@ -10,3 +10,10 @@ library(readr)
 #read in data set
 oly = read_csv("Olympics.csv")
 
+#7a. Which countries had the largest delegation of athletes in 1992? Create a tibble that
+# contains only the variables country and athletes.
+most.athletes = oly %>% 
+  filter(year == 1992) %>% 
+  select(country, athletes) %>% 
+  arrange(desc(athletes))
+most.athletes
